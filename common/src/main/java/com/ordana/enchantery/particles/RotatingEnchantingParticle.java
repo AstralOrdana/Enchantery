a8143d1bcf17f70f151dcd9f7626cebfafe689c2
Ordana <astralordana@gmail.com>
2023-06-09 15:58:53 -0500

    omg i never pushed the perfect particle rotation??? shit i'm glad i didn't lose my laptop or something, christ

--- common/src/main/java/com/ordana/enchantery/particles/RotatingEnchantingParticle.java
+++ common/src/main/java/com/ordana/enchantery/particles/RotatingEnchantingParticle.java
@@ -1,5 +1,7 @@
 package com.ordana.enchantery.particles;
 
+import io.netty.util.internal.MathUtil;
+import net.minecraft.Util;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.*;
 import net.minecraft.core.particles.SimpleParticleType;
@@ -13,6 +15,7 @@ public class RotatingEnchantingParticle extends TextureSheetParticle {
     private final double startX;
     private final double startY;
     private final double startZ;
+    private final double toCenterAngle;
 
     protected RotatingEnchantingParticle(ClientLevel clientLevel, double x, double y, double z,
                                          double targetX, double targetY, double targetZ) {
@@ -23,36 +26,31 @@ public class RotatingEnchantingParticle extends TextureSheetParticle {
         this.targetX = targetX;
         this.targetY = targetY;
         this.targetZ = targetZ;
+        this.toCenterAngle = (Math.atan2(targetZ - startZ, targetX - startX) + Math.PI / 2);
         this.gravity = 0;
         this.hasPhysics = false;
         this.lifetime = 500;
     }
 
 
-    public static Vec3 spiralMotion(Vec3 startPosition, Vec3 center, double radius, double angularVelocity, double time) {
-
-        var t = (Mth.clamp(time, 0, Math.PI * 2)) / (Math.PI * 2);
-        var x = Mth.lerp(t, startPosition.x, center.x + Math.cos(time) * radius);
-        var y = Mth.lerp(t, startPosition.y, center.y);
-        var z = Mth.lerp(t, startPosition.z, center.z + Math.sin(time) * radius);
-        return new Vec3(x, y, z);
-
-    }
-
     @Override
     public void tick() {
         super.tick();
-        // Calculate radius vector and distance from center
-        Vec3 pos = new Vec3(x, y, z);
+
         Vec3 startPos = new Vec3(startX, startY, startZ);
         Vec3 center = new Vec3(targetX, targetY - 0.5, targetZ);
-
-        //double radius = pos.subtract(center).length();
         double targetRadius = 1.25;
-        //double tT = 5*20;
 
-        Vec3 v = spiralMotion(startPos, center, targetRadius, Math.PI / 180, Math.toRadians(this.age) + Math.atan2(targetZ - startZ, targetX - startX) + Math.PI);
-        this.setPos(v.x, v.y, v.z);
+
+        var t = Mth.clamp(Math.toRadians(age) / (Math.PI * 2), 0, 1);
+
+        var time = (Math.toRadians(age) / t) + (toCenterAngle);
+
+        var x = Mth.lerp(t, startPos.x, center.x + Math.cos(time) * -1 * targetRadius);
+        var y = Mth.lerp(t, startPos.y, center.y);
+        var z = Mth.lerp(t, startPos.z, center.z + Math.sin(time) * -1 * targetRadius);
+
+        this.setPos(x, y, z);
     }
 
     @Override
